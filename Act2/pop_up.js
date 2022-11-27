85ea6d353de2123670d5f1e24bab28b890233c5b
Devz0118 <91242832+Devz0118@users.noreply.github.com>
2022-11-28 02:11:52 +0800

    Add files via upload

---
+++ Act2/pop_up.js
@@ -0,0 +1,10 @@
+function openpopup() {
+    var element = document.getElementById("popup");
+    element.classList.add("active");
+}
+
+ function closepopup() {
+    var element = document.getElementById("popup");
+    element.classList.remove("active");
+}
+ 
